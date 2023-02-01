60a2e1edb013aff7c2ce16e08aeadb3aa1779b36
Nnamdi Okeke <onnamdialex@gmail.com>
2023-02-01 15:08:07 +0100

    :art: Improve structure of the code

--- payment/src/App.jsx
+++ payment/src/App.jsx
@@ -1,8 +1,21 @@
 import React from "react";
+import styles from "./style";
 
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
-    <h1>App</h1>
+    <div className={`${styles.padding} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>Navbar</div>
+    </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>Hero</div>
+    </div>
+
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        Stats Business Billing CardDeal Testimonials Clients CTA Footer
+      </div>
+    </div>
   </div>
 );
 
