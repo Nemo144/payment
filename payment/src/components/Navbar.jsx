3ed81dbfbbf7e0261aadddf904f986e3c57f9857
Nnamdi Okeke <onnamdialex@gmail.com>
2023-02-01 16:55:49 +0100

    :construction: navbar wip ...

--- payment/src/components/Navbar.jsx
+++ payment/src/components/Navbar.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { close, logo, menu } from "../assets";
+import { navLinks } from "../constants";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="w-4 flex py-6 justify-between items-center navbar">
+      <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
+
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+        {navLinks.map((navLink, index) => (
+          <li
+            key={navLink.id}
+            className={`font-poppins font-normal cursor-pointer text-[16px] ${
+              index === navLinks.length - 1 ? "mr-0" : "mr-10"
+            } text-white`}
+          >
+            <a href={`#${navLink.id}`}>{navLink.title}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
